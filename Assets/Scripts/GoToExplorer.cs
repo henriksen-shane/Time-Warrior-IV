0249eeb570571020a5e5e2b85de8a94d8030aee7
henriksen-shane <henriksen.shane@gmail.com>
2016-02-22 16:24:38 -0700

    Update GoToExplorer.cs

--- Assets/Scripts/GoToExplorer.cs
+++ Assets/Scripts/GoToExplorer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GoToExplorer : MonoBehaviour {
 
+	//move to the explorer explanation screen
 	public void Clicked (){
 		Application.LoadLevel ("Explorer");
 	}
