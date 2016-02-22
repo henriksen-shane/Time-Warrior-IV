1f5919015a8f4defef4f9c251ff180bb3b3e128e
henriksen-shane <henriksen.shane@gmail.com>
2016-02-22 16:25:00 -0700

    Update GoToChaos.cs

--- Assets/Scripts/GoToChaos.cs
+++ Assets/Scripts/GoToChaos.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GoToChaos : MonoBehaviour {
 
-	//move ot the chaos explanation screen
+	//move to the chaos explanation screen
 	public void Clicked (){
 		Application.LoadLevel ("Chaos");
 	}
