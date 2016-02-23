1499bccb98717fed1b8694f46e518feedde5c7e3
henriksen-shane <henriksen.shane@gmail.com>
2016-02-22 17:05:56 -0700

    Update GoToStory.cs

--- Assets/Scripts/GoToStory.cs
+++ Assets/Scripts/GoToStory.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class GoToStory : MonoBehaviour {
-
+//move to the story explanation screen
 	public void Clicked (){
 		Application.LoadLevel ("StoryIntro");
 	}
