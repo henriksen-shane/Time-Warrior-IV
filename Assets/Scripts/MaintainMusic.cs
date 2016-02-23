43758a14c821d72a25599b66dcaeb5d14dd8b609
henriksen-shane <henriksen.shane@gmail.com>
2016-02-23 16:54:33 -0700

    Update MaintainMusic.cs

--- Assets/Scripts/MaintainMusic.cs
+++ Assets/Scripts/MaintainMusic.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MaintainMusic : MonoBehaviour {
 
-
+	//keeps the intro music playing until the main game screen is loaded
 	void Awake () {
 		DontDestroyOnLoad(this.gameObject);
 	}
