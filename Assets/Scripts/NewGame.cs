782dd976d91201ca878ce3cf733aa97c07e54b32
henriksen-shane <henriksen.shane@gmail.com>
2016-02-23 17:04:22 -0700

    Update NewGame.cs

--- Assets/Scripts/NewGame.cs
+++ Assets/Scripts/NewGame.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class NewGame : MonoBehaviour {
 
+// reloads the main game scene, starting a new game. Called from option menu
 	public void Clicked () {
 		Application.LoadLevel(Application.loadedLevel);
 	}
